eae1196e5983d33c9559c823e133c7cc9be7b964
khattab88 <m_khatab_88@hotmail.com>
2023-11-02 20:40:22 +0200

    create bank account class

---
+++ Sparky.Tests.NUnit/BankAccountTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky.Tests.NUnit
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+
+    }
+}
---
+++ Sparky/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class BankAccount
+    {
+        private int _balance { get; set; }
+
+        public BankAccount()
+        {
+            _balance = 0;
+        }
+
+        public bool Deposit(int amount)
+        {
+            _balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if(amount <= _balance)
+            {
+                _balance -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetBalance() { return _balance; }
+    }
+}
