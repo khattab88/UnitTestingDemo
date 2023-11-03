8817aea6e84d9c1d4abcaaa209ce8bf7aff29173
khattab88 <m_khatab_88@hotmail.com>
2023-11-03 15:54:43 +0200

    implement product xunit tests

---
+++ Sparky.Tests.XUnit/ProductTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sparky.Tests.XUnit
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void GetPrice_IsPremiumCustomer_ReturnsPriceWith20Discount()
+        {
+            Product product = new() { Price = 100 };
+
+            var result = product.GetPrice(new Customer { IsPremium = true });
+
+            Assert.Equal(80, result);
+        }
+    }
+}
