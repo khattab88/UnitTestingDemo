81d6b3e281e5a58b0dfe7c8fee9cb3868d6d795c
khattab88 <m_khatab_88@hotmail.com>
2023-11-03 16:40:38 +0200

    update solution structure

---
+++ Bongo_InitialSetup (.NET 6)/Bongo.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bongo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
