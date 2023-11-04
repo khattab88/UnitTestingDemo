bf6e2423927164d3f4df9e1a24dcef2706598594
khattab88 <m_khatab_88@hotmail.com>
2023-11-04 23:50:33 +0200

    testing app service - GetAll()

---
+++ Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Bongo.Core.Services;
+using Bongo.DataAccess.Repository.IRepository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Core.Tests
+{
+    [TestFixture]
+    public class StudyRoomBookingServiceTests
+    {
+        private Mock<IStudyRoomRepository> _roomRepoMock;
+        private Mock<IStudyRoomBookingRepository> _bookingRepoMock;
+        private StudyRoomBookingService _bookingSvc;
+
+        [SetUp]
+        public void Setup()
+        {
+            _roomRepoMock = new Mock<IStudyRoomRepository>();
+            _bookingRepoMock = new Mock<IStudyRoomBookingRepository>();
+            _bookingSvc = new StudyRoomBookingService(_bookingRepoMock.Object, _roomRepoMock.Object);
+        }
+
+        [TestCase]
+        public void GetAll_Invoke_VerifyMethodIsCalled()
+        {
+            _bookingSvc.GetAllBooking();
+
+            _bookingRepoMock.Verify(x => x.GetAll(null), Times.Once);
+        }
+    }
+}
