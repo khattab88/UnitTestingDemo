27d419b537f844d7b8e5f0e8b0729e0a826fe7c7
khattab88 <m_khatab_88@hotmail.com>
2023-11-04 23:15:47 +0200

    testing repositories - GetAll()

--- Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
+++ Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -3,6 +3,7 @@ using Bongo.Models.Model;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +14,14 @@ namespace Bongo.DataAccess.Tests
     [TestFixture]
     public class StudyRoomBookingRepositoryTests
     {
+        private DbContextOptions<ApplicationDbContext> _dbContextOptions;
+
         private StudyRoomBooking StudyRoomBookingOne;
         private StudyRoomBooking StudyRoomBookingTwo;
 
         public StudyRoomBookingRepositoryTests()
         {
-            StudyRoomBookingOne = new() 
+            StudyRoomBookingOne = new()
             {
                 FirstName = "Ben",
                 LastName = "Stark",
@@ -39,15 +42,22 @@ namespace Bongo.DataAccess.Tests
             };
         }
 
+        [SetUp]
+        public void SetUp()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "temp_bongo").Options;
+        }
+
+
         [Test]
+        [Order(1)]
         public void Book_WithStudyRoomBookingOne_CheckValuesFromDb()
         {
             // Arrange
-            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "temp_bongo").Options;
-
+            
             // Act
-            using (var context = new ApplicationDbContext(dbContextOptions)) 
+            using (var context = new ApplicationDbContext(_dbContextOptions))
             {
                 var repository = new StudyRoomBookingRepository(context);
 
@@ -55,7 +65,8 @@ namespace Bongo.DataAccess.Tests
             }
 
             // Assert
-            using (var context = new ApplicationDbContext(dbContextOptions)){
+            using (var context = new ApplicationDbContext(_dbContextOptions))
+            {
                 var bookingFromDb = context.StudyRoomBookings.FirstOrDefault(b => b.BookingId == 11);
 
                 Assert.IsNotNull(bookingFromDb);
@@ -66,6 +77,49 @@ namespace Bongo.DataAccess.Tests
                 Assert.AreEqual(StudyRoomBookingOne.Email, bookingFromDb?.Email);
                 Assert.AreEqual(StudyRoomBookingOne.Date, bookingFromDb?.Date);
             }
+        }
+
+        [Test]
+        [Order(2)]
+        public void GetAll_WithBookingOneAndTwo_CheckBothBookingsFromDb()
+        {
+            // Arrange
+            List<StudyRoomBooking> expected = new() { StudyRoomBookingOne, StudyRoomBookingTwo };
+
+            using (var context = new ApplicationDbContext(_dbContextOptions))
+            {
+                context.Database.EnsureDeleted();
+
+                var repository = new StudyRoomBookingRepository(context);
+
+                repository.Book(StudyRoomBookingOne);
+                repository.Book(StudyRoomBookingTwo);
+            }
+
+            // Act
+            List<StudyRoomBooking> result = new();
+            using (var context = new ApplicationDbContext(_dbContextOptions))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+
+                result = repository.GetAll(null).ToList();
+            }
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result, new StudyRoomBookingComparer());
+        }
+    }
+
+    class StudyRoomBookingComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            StudyRoomBooking booking1 = (StudyRoomBooking)x;
+            StudyRoomBooking booking2 = (StudyRoomBooking)y;
+
+            if (booking1?.BookingId != booking2?.BookingId) return 1;
+
+            return 0;
         }
     }
 }
