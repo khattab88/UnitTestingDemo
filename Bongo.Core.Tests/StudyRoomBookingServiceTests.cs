88d6fae915f49e76a30d04b1c57c8d5eb61215eb
khattab88 <m_khatab_88@hotmail.com>
2023-11-05 00:17:33 +0200

    TEST - save booking

--- Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
+++ Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Bongo.Core.Services;
 using Bongo.DataAccess.Repository.IRepository;
+using Bongo.Models.Model;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -17,9 +18,28 @@ namespace Bongo.Core.Tests
         private Mock<IStudyRoomBookingRepository> _bookingRepoMock;
         private StudyRoomBookingService _bookingSvc;
 
+        private StudyRoomBooking _request;
+        private List<StudyRoom> _availableRooms;
+
         [SetUp]
         public void Setup()
         {
+            _request = new()
+            {
+                FirstName = "Ben",
+                LastName = "Stark",
+                Email = "bstark@gmail.com",
+                Date = new DateTime(2024, 1, 1)
+            };
+
+            _availableRooms = new List<StudyRoom>{
+                new StudyRoom
+                {
+                    Id = 1,
+                    RoomName = "Regular",
+                    RoomNumber = "A101"
+                }};
+
             _roomRepoMock = new Mock<IStudyRoomRepository>();
             _bookingRepoMock = new Mock<IStudyRoomBookingRepository>();
             _bookingSvc = new StudyRoomBookingService(_bookingRepoMock.Object, _roomRepoMock.Object);
@@ -31,6 +51,43 @@ namespace Bongo.Core.Tests
             _bookingSvc.GetAllBooking();
 
             _bookingRepoMock.Verify(x => x.GetAll(null), Times.Once);
+        }
+
+        [TestCase]
+        public void BookStudyRoom_InputNullRequest_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => _bookingSvc.BookStudyRoom(null));
+
+            //Assert.AreEqual("Value cannot be null. (Parameter 'request')", exception.Message);    
+            Assert.AreEqual("request", exception.ParamName);
+        }
+
+        [TestCase]
+        public void BookStudyRoom_BookWithAvailableRoom_ReturnsResultWithAllValues()
+        {
+            // arrange
+            StudyRoomBooking savedStudyRoomBooking = null;
+
+            _roomRepoMock.Setup(x => x.GetAll()).Returns(_availableRooms);
+            _bookingRepoMock.Setup(x => x.Book(It.IsAny<StudyRoomBooking>()))
+                .Callback<StudyRoomBooking>(booking =>
+                {
+                    savedStudyRoomBooking = booking;
+                });
+
+            // act
+            _bookingSvc.BookStudyRoom(_request);
+
+            // assert
+            _bookingRepoMock.Verify(x => x.Book(It.IsAny<StudyRoomBooking>()), Times.Once);
+
+            Assert.NotNull(savedStudyRoomBooking);
+            Assert.AreEqual(_request.FirstName, savedStudyRoomBooking.FirstName);
+            Assert.AreEqual(_request.LastName, savedStudyRoomBooking.LastName);
+            Assert.AreEqual(_request.Email, savedStudyRoomBooking.Email);
+            Assert.AreEqual(_request.Date, savedStudyRoomBooking.Date);
+            Assert.AreEqual(_availableRooms.First().Id, savedStudyRoomBooking.StudyRoomId);
         }
     }
 }
